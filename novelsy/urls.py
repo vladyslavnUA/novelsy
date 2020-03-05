c65881299cfa2ab4c051176d9a00d2e6995370d0
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-05 00:42:46 -0800

    updated app urls

--- novelsy/urls.py
+++ novelsy/urls.py
@@ -19,10 +19,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-
-]
+    path('register/', include('register.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
