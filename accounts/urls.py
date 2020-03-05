e9049eef8cecb379ccf6eee96cadddc403063002
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-05 00:36:24 -0800

    updated urls/views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from project.views import BookListView
 from accounts.views import SignUpView
 
 
-
+app_name = 'accounts'
 
 urlpatterns = [
     path('list_of_books/', BookListView.as_view(), name='accounts'),
-    path('signup/', SignUpView.as_view(), name='templates-signup-page'),
+    path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,95 @@
-# Learn more or give us feedback
 from django.shortcuts import render
 from accounts.forms import UserCreationForm
+from accounts.forms import SignUpForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.detail import (DetailView)
+from django.contrib.auth.models import User
+from django.views.generic.edit import (
+    CreateView,
+    UserProfile
+)
 
 
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+class SignUpView(SuccessMessageMixin, CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('accounts:login')
     template_name = 'registration/signup.html'
+    success_message = (
+        '''congradulations! you are now a registered user.'''
+    )
+    
+    def form_valid(self, form):
+        self.object = form.save()
+        return super().form_valid(form)
+
+
+class UserProfile(UserPassesTestMixin, DetailView):
+    model = User
+    template_name = 'accounts/profile/show_profile.html'
+
+    def get_queryset(self):
+        '''Returns a queryset of all User objects.'''
+        return self.model.objects.all()
+
+    def get(self, request, pk):
+        """Renders a page to show an account of user.
+           Parameters:
+           pk(int): specific id of the User in db.
+           request(HttpRequest): the HTTP request sent to the server
+
+           Returns:
+           render: HttpResponse
+
+         """
+        user = self.get_queryset().get(id=pk)
+        context = {
+            'user': user,
+        }
+        return render(request, self.template_name, context)
+
+    def test_func(self):
+        '''Ensures the user can see only their own profile.'''
+        requested_user = self.get_object()
+        user = self.request.user
+        return requested_user == user
+
+
+# class ProfileUpdate(UserPassesTestMixin, UpdateView):
+#     model = ArchitectOrOfficer
+#     form_class = StatusForm
+#     template_name = 'accounts/profile/update.html'
+
+#     def get_queryset(self):
+#         '''Returns a queryset of all User objects.'''
+#         return UserProfile.objects.all()
+
+#     def get(self, request, pk):
+#         """Renders a page for user to check off if they're an architect or
+#            compliance officer.
+
+#            Parameters:
+#            pk(int): specific id of the User in db.
+#            request(HttpRequest): the HTTP request sent to the server
+
+#            Returns:
+#            render: HttpResponse
+
+#          """
+#         status = self.get_queryset().get(id=pk)
+#         user = User.objects.get(id=status.user.id)
+#         context = {
+#             'user': user,
+#         }
+#         return super().get(request)
+
+#     def test_func(self):
+#         '''Ensures the user can see only their own profile.'''
+#         requested_user = self.get_object().user
+#         user = self.request.user
+#         return requested_user == user
+# class ProfileDelete(self):
+#     pass
