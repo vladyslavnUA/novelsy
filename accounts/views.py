3e07350d3023f89055cccad03beb35147908f906
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-04 21:57:25 -0800

    added home function

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-Learn more or give us feedback
+# Learn more or give us feedback
 from django.shortcuts import render
 from accounts.forms import UserCreationForm
 from django.urls import reverse_lazy
@@ -10,5 +10,3 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
-
-# Create your views here.
