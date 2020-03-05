ef4f14593e5d00871830835d8ca7763aa42312e0
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-05 00:35:18 -0800

    updated signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,17 +2,35 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
-class UserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, label='Email')
-
+class SignUpForm(UserCreationForm):
+    '''A form that handles registering new users.'''
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ['email', 'username',
+                  'first_name', 'last_name',
+                  'password1', 'password2'
+                  ]
 
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
+        '''Initializes fields of the new User instance.'''
+        user = super(SignUpForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
+
+# class UserCreationForm(UserCreationForm):
+#     email = forms.EmailField(required=True, label='Email')
+
+#     class Meta:
+#         model = User
+#         fields = ("username", "email", "password1", "password2")
+
+#     def save(self, commit=True):
+#         user = super(UserCreationForm, self).save(commit=False)
+#         user.email = self.cleaned_data["email"]
+#         if commit:
+#             user.save()
+#         return user
