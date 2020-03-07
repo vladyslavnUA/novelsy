87335b078030428aeda294caddbcb30c9eb1287c
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-06 23:07:07 -0800

    updated app urls

--- novelsy/urls.py
+++ novelsy/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path('register/', include('register.urls')),
+] 
+#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
