fb91e64fe25124ab3e57cf58c0f1691791ab1839
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-06 18:07:01 -0800

    updated urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,11 +4,11 @@ from project.views import BookListView
 from accounts.views import SignUpView
 
 
-app_name = 'accounts'
+# app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
+    # path('accounts/', include('accounts.urls')),
 ]
