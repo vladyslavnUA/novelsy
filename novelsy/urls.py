20d91cf9216e4166b0264134db4f6f72265794a5
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2020-03-11 17:28:38 -0700

    updated urls

--- novelsy/urls.py
+++ novelsy/urls.py
@@ -1,7 +1,7 @@
-"""novelsy URL Configuration
+"""books URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,12 +19,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    # path('register/', include('register.urls')),
-] 
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('api/', include('api.urls')),
+    path('payments/', include('payments.urls')),
+]
