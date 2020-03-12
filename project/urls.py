1ca2e8f653808a282581b6552ef0063d1873aac1
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2020-03-11 17:31:54 -0700

    updated urls

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,13 @@
-
 from django.urls import path, include
-from project.views import Home, BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from project.views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView, Home
 
-# app_name = 'project'
 
 urlpatterns = [
-    path('', Home.as_view(), name='home_page'),
+    path('', Home.as_view(), name='home-page'),
     path('list_of_books/', BookListView.as_view(), name='book-list-project'),
-    path('new_book/', BookCreateView.as_view(), name='book-create-project'),
+    path('new_project/', BookCreateView.as_view(), name='book-create-project'),
     path('<str:slug>/', BookDetailView.as_view(), name='book-details-project'),
     path('edit/<int:pk>/', BookUpdateView.as_view(), name='book-update-project'),
     path('delete/<int:pk>', BookDeleteView.as_view(), name='book-delete-project'),
+
 ]
